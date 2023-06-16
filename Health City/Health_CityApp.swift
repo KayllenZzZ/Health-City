5f51b425c2051b861eaa885ebe73828eca48cdcc
Dusha <56073470+KayllenZzZ@users.noreply.github.com>
2023-06-16 21:21:45 +0300

    Initial Commit

---
+++ Health City/Health_CityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_CityApp.swift
+//  Health City
+//
+//  Created by Андрей Чередник on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Health_CityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
